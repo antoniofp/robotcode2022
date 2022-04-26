0aca69a05c064d6d5f8cec3d9c2f9e7ce124a512
antoniofp <j.antoniofdez89@gmail.com>
2022-04-25 21:23:31 -0500

    Climber encoder limits, control to turn off compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxRelativeEncoder.Type;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -23,22 +24,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 // Creating objects for controllers
-  private final XboxController m_driverController = new XboxController(0);
-  private final XboxController m_driver2Controller = new XboxController(1);
+  final XboxController m_driverController = new XboxController(0);
+  final XboxController m_driver2Controller = new XboxController(1);
 //Creating objects for chassis motors
-  private CANSparkMax m_frontleftMotor = new CANSparkMax(1, MotorType.kBrushless);
-  private CANSparkMax m_backleftMotor = new CANSparkMax(2, MotorType.kBrushless);
-  private CANSparkMax m_frontrightMotor = new CANSparkMax(3, MotorType.kBrushless);
-  private CANSparkMax m_backrightMotor = new CANSparkMax(4, MotorType.kBrushless);
+  CANSparkMax m_frontleftMotor = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax m_backleftMotor = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax m_frontrightMotor = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax m_backrightMotor = new CANSparkMax(4, MotorType.kBrushless);
 //Grouping Left chassis motors and right chassis motors
-  private final MotorControllerGroup leftMotors = new MotorControllerGroup(m_frontleftMotor, m_backleftMotor);
-  private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_frontrightMotor, m_backrightMotor);
+  final MotorControllerGroup leftMotors = new MotorControllerGroup(m_frontleftMotor, m_backleftMotor);
+  final MotorControllerGroup rightMotors = new MotorControllerGroup(m_frontrightMotor, m_backrightMotor);
 //Creating objects for mechanims motors
-  private CANSparkMax shooter = new CANSparkMax(5, MotorType.kBrushless);
-  private CANSparkMax intake = new CANSparkMax (6, MotorType.kBrushless);
-  private CANSparkMax feeder = new CANSparkMax (7, MotorType.kBrushless);
-  private CANSparkMax leftClimber = new CANSparkMax (8, MotorType.kBrushless);
-  private CANSparkMax rightClimber = new CANSparkMax (9, MotorType.kBrushless);
+  CANSparkMax shooter = new CANSparkMax(5, MotorType.kBrushless);
+  CANSparkMax intake = new CANSparkMax (6, MotorType.kBrushless);
+  CANSparkMax feeder = new CANSparkMax (7, MotorType.kBrushless);
+  CANSparkMax leftClimber = new CANSparkMax (8, MotorType.kBrushless);
+  CANSparkMax rightClimber = new CANSparkMax (9, MotorType.kBrushless);
 // Create object for drive train
   DifferentialDrive m_myRobot = new DifferentialDrive(rightMotors, leftMotors);
 // Create encoder objects 
@@ -49,7 +50,8 @@ public class Robot extends TimedRobot {
   RelativeEncoder shootEncoder = shooter.getEncoder(Type.kHallSensor, 42);
   RelativeEncoder feederEncoder = feeder.getEncoder(Type.kHallSensor, 42);
 // Create solenoid object
-  private final DoubleSolenoid doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  final DoubleSolenoid doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  final Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
   
   @Override
   public void robotInit() { //Class used when the robot is on
@@ -59,13 +61,14 @@ public class Robot extends TimedRobot {
     rightClimber.follow(leftClimber);
     leftClimber.stopMotor();
     rightClimber.stopMotor(); 
+    leftClimbEncoder.setPosition(0);
   }
   @Override
   public void robotPeriodic() { //Class used all time
     SmartDashboard.putNumber("Left Wheels Pos", leftEncoder.getPosition());
     SmartDashboard.putNumber("Right Wheels Pos", rightEncoder.getPosition());
     SmartDashboard.putNumber("Shooter Temp", shooter.getMotorTemperature());
-    SmartDashboard.putNumber("Climber L", rightClimbEncoder.getPosition());
+    SmartDashboard.putNumber("Climber L", leftClimbEncoder.getPosition());
     SmartDashboard.putNumber("Intake Temp", intake.getMotorTemperature());
   }
 
@@ -116,6 +119,7 @@ public class Robot extends TimedRobot {
     m_frontrightMotor.setIdleMode(IdleMode.kBrake);
     leftClimber.setIdleMode(IdleMode.kBrake);
     rightClimber.setIdleMode(IdleMode.kBrake);
+    leftClimbEncoder.setPosition(0);
   }
   @Override
   public void teleopPeriodic() { //Class used during the teleoperated period 
@@ -133,16 +137,36 @@ public class Robot extends TimedRobot {
     double reverseFeederSpeed = (m_driver2Controller.getYButton()) ? -0.2 : 0;
     double climbUpSpeed = (m_driverController.getRightBumper()) ? 0.4 : 0;
     double climbDownSpeed = (m_driverController.getLeftBumper()) ? -0.8 : 0;
+// Enable lock mechanism when climber reaches 34 revolutions
+    boolean lockEnabled = false;
+    if (leftClimbEncoder.getPosition() >= 34) {
+      lockEnabled = true;
+    }
 // Speeds for motors are set using the previous variables
     shooter.set(shooterSpeed);
     intake.set(intakeSpeed + reverseIntakeSpeed);
     feeder.set(feederSpeed + reverseFeederSpeed);
-    leftClimber.set(climbUpSpeed + climbDownSpeed);
+    // if climber is within that range, it can move up or down
+    if (leftClimbEncoder.getPosition() >= 1 & leftClimbEncoder.getPosition() <=34) {
+      leftClimber.set(climbUpSpeed + climbDownSpeed);
+    } // if the climber is below 1, and lock is not enabled, it can only go up
+      else if (leftClimbEncoder.getPosition()<1 & !lockEnabled) {
+        leftClimber.set(climbUpSpeed);
+      // if climber lock gets enabled and it is below 34, it can move up or down, including below 0
+      } else if (lockEnabled & leftClimbEncoder.getPosition() < 34) {
+      leftClimber.set(climbDownSpeed + climbUpSpeed);
+      } else { // if the climber is not in the previous cases, it means it is above 34, so it must only go down
+      leftClimber.set(climbDownSpeed);
+    }
 // Intake is lowered or elevated with D-Pad on controller
     if (m_driver2Controller.getPOV() == 0) {
     doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-    } else if (m_driver2Controller.getPOV() == 180) {
+    }
+    if (m_driver2Controller.getPOV() == 180) {
       doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+    if (m_driver2Controller.getRightBumperPressed()){
+      compressor.disable();
     }
   }
 }
